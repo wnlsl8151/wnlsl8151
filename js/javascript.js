d22dff672a9637474e28a61f40a61e7ec383686e
wnlsl8151 <128011468+wnlsl8151@users.noreply.github.com>
2024-01-27 09:29:18 +0900

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,11 @@
+function navBtn() {
+  document.getElementById('M-subnav').style.display = 'block';
+  document.getElementById('M-btn').style.display = 'none';
+  document.getElementById('M-btn-c').style.display = 'block';
+}
+
+function navBtnC() {
+  document.getElementById('M-subnav').style.display = 'none';
+  document.getElementById('M-btn-c').style.display = 'none';
+  document.getElementById('M-btn').style.display = 'block';
+}
